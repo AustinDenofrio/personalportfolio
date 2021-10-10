c746e2e504dcaf5dabb2ac74307b128b73682518
Austin Denofrio <denofrioaustin@gmail.com>
2021-10-09 20:06:14 -0400

    added js notes to js page

---
+++ js/script.js
@@ -0,0 +1,42 @@
+
+
+/* variables */
+/* the var variable notation acts as a scriptwide accessible variable.
+ its rarely used. let is the standard practice use for variables*/
+ /*
+var nameOfVar = "itsValue"
+*/
+/*contstants are values that once made cannot be changed*/
+/*
+const nameOfVar = "itsValue"
+*/
+/*standard practice for creating variables */
+/*
+let nameOfVar = "itsValue"
+*/
+/*you  can also make it an object which means it has multiple properties
+ and can even have nested funtions*/
+ /*
+ let nameOfVar = {
+     property1: itsValue,
+     property2: someValue,
+     nameOfFuntion: function() {
+         typeOfFuntion(this.targetVariable)
+     }
+ }
+*/
+/*Something to notice for syntex is the this. before the target
+ in the funtion is necessary to target the funtions inside the object itsef.
+ it will not work properly without that syntex*/
+
+
+/*funtions*/
+
+/*
+  function() {
+    typeOfFuntion(target variable);
+}
+*/
+
+
+
