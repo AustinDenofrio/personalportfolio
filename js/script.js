4e80be65a3ce07bff5de28a6ed725c97aacff2fd
Austin_Denofrio_Macbook_Air <denofrioaustin@gmail.com>
2021-10-13 20:00:14 -0400

    Touched-up html layout, added footer

--- js/script.js
+++ js/script.js
@@ -66,7 +66,7 @@ button.addEventListener('click', function() {
     }
 } )
 */
-const button = document.getElementById('see-review');
+const button = document.getElementById('hamburgerBtn');
 
 button.addEventListener('click', function() {
     const showingDiv = document.getElementById('displayNav');
